1195415636124c7690561459bbf485e95af26ee5
Lewis Dale <lewis.dale@fortacode.co.uk>
2022-12-16 22:24:36 +0000

    Parse RSS feed and add to global data

---
+++ src/index.js
@@ -0,0 +1,51 @@
+const EleventyFetch = require("@11ty/eleventy-fetch");
+const Parser = require('rss-parser');
+
+const parser = new Parser();
+
+const getUrl = async (url) => await EleventyFetch(`${url}.json`, {
+	duration: "1w",
+	type: "json"
+});
+
+const getAuthor = async (url) => {
+	const author = await getUrl(url);
+	
+	return author.name;
+}
+
+const getBook = async (url) => {
+	const bookData = await getUrl(url);
+
+	if (bookData) {
+		const authors = await Promise.all(bookData.authors.map(getAuthor));
+		
+		return {
+			title: bookData.title,
+			authors,
+			description: bookData.description,
+			publishedDate: new Date(bookData.publishedDate),
+			cover: bookData.cover.url,
+		}
+	}
+}
+
+const getMessage = async (feedItem) => {
+	console.log(feedItem);
+	const message = await getUrl(feedItem.link);
+	const book = await getBook(message.tag[0].href);
+
+	return {
+		book,
+		date: new Date(message.published),
+		status: feedItem.contentSnippet,
+	}
+}
+
+module.exports = function(eleventyConfig, options) {
+	eleventyConfig.addGlobalData("bookwyrm", async () => {
+		const url = `${options.domain}/user/${options.user}/rss`;
+		const feed = await parser.parseURL(url);
+		return await Promise.all(feed.items.map(getMessage));
+	});
+}
